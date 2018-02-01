a70225e8fffccf94ea8d1e355c84528a7fa79d6a
caihedong <caihedongorder@126.com>
2018-02-01 16:55:22 +0800

    整理项目

--- build_compile_data.bat
+++ build_compile_data.bat
@@ -1,3 +1,4 @@
-cmake -G "NMake Makefiles"  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-del Makefile
-del cmake_install.cmake
+@cmake -G "NMake Makefiles"  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+@del Makefile
+@del cmake_install.cmake
+@del CMakeCache.txt
